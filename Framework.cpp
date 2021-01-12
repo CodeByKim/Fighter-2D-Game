7f304344ff2400b361e84cd7dc3f30a0cb095ec7
Garam Kim <codebykim@naver.com>
2021-01-12 17:13:00 +0900

    WinMain 수정

--- Framework.cpp
+++ Framework.cpp
@@ -1,12 +1,128 @@
 #include "Framework.h"
 
+BOOL Framework::Create(HINSTANCE hInstance, int nCmdShow)
+{
+    m_hInstance = hInstance;
+
+    RegisterWindowClass();
+    
+    return CreateCreateWindowInstance(nCmdShow);
+}
+
+VOID Framework::Run()
+{
+    MSG msg;
+    while (TRUE)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            FrameUpdate();
+        }
+    }
+}
+
+VOID Framework::Release()
+{
+    //ReleaseDC(g_hWnd, g_hDc);
+}
+
 Framework& Framework::GetInstance()
 {
 	static Framework instance;
 	return instance;
 }
 
-Framework::Framework()
+VOID Framework::RegisterWindowClass()
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = Framework::WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = m_hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = WINDOWCLASS_NAME;
+    wcex.hIconSm = NULL;
+
+    RegisterClassExW(&wcex);
+}
+
+BOOL Framework::CreateCreateWindowInstance(int nCmdShow)
+{   
+    HWND hWnd = CreateWindowW(WINDOWCLASS_NAME, WINDOWCLASS_NAME, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, m_hInstance, nullptr);
+
+    if (hWnd == NULL)
+    {
+        return FALSE;
+    }
+
+    m_hWnd = hWnd;
+
+    RECT rectWindow = { 0, 0, 800, 600 };
+    AdjustWindowRect(&rectWindow, WS_OVERLAPPEDWINDOW, FALSE);
+    SetWindowPos(hWnd, 
+                HWND_TOPMOST, 
+                100, 
+                100, 
+                rectWindow.right - rectWindow.left, 
+                rectWindow.bottom - rectWindow.top, 
+                SWP_NOMOVE | SWP_NOZORDER);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+VOID Framework::FrameUpdate()
+{
+    OutputDebugString(L"Update...\n");    
+}
+
+LRESULT CALLBACK Framework::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        hdc = BeginPaint(hWnd, &ps);
+        //BitmapDraw(hdc);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+Framework::Framework() 
+    : m_hInstance(NULL), m_hWnd(NULL)
+{
+}
+
+Framework::~Framework()
 {
 
 }
--- framework.h
+++ framework.h
@@ -2,10 +2,26 @@
 
 #include "Library.h"
 
+#define WINDOWCLASS_NAME L"TCPFighter"
+
 class Framework
 {
-public:
-	static Framework& GetInstance();
+public:	
+	BOOL						Create(HINSTANCE hInstance, int nCmdShow);
+	VOID						Run();
+	VOID						Release();
+	static Framework&			GetInstance();
+
 private:
+	HINSTANCE					m_hInstance;
+	HWND						m_hWnd;
+
+	VOID						RegisterWindowClass();
+	BOOL						CreateCreateWindowInstance(int nCmdShow);
+	VOID						FrameUpdate();
+
+	static LRESULT CALLBACK		WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 	Framework();
+	~Framework();
 };
